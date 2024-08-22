d5f8e1877b3c26353ded74774c822ce8eb43c4e0
Tariq Mir <tariq.mir@dci-student.org>
2024-08-22 14:09:08 +0200

    lesson Aug 22

---
+++ apps/book/serializer/author.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from apps.book.models import Author
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    #TODO: specify the model that this serializer will link to
+    # TODO: Specify which fields should be considered in the model
+
+    # Force django REST to recognize the method
+    name = serializers.CharField() # read-only
+    # create a serialized method
+    username = serializers.SerializerMethodField()
+
+    # Serialized method's implementation
+    def get_username(self, obj): # get <serializer_method_field>
+        return '_'.join([obj.first_name, obj.last_name])
+
+    def validate_first_name(self, value): # validate_<field-name>
+        """Field-level Validation"""
+        if '-' in value:
+            # TODO: Always raise a validation exception when condition fails
+            raise serializers.ValidationError('First name should not contain hyphen (-)')
+        
+        # TODO: If condition is true, then return the value
+        return value
+
+
+    def validate(self, attrs):
+        """Object-level Validation"""
+        if attrs.get('first_name') == attrs.get('last_name'):
+            raise serializers.ValidationError('First name and last name should not be the same')
+        
+        return attrs
+
+ 
+    class Meta:
+        model = Author
+        fields = '__all__' # ('id', 'first_name')
+        read_only_fields = ('id', )
+
+
---
+++ apps/book/serializer/tag.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from apps.book.models import Tag
+import re
+
+
+class TagSerializer(serializers.ModelSerializer):
+
+    #  def validate_name(self, value):
+     #   if re.search(r'[%!@#$^&*]', value):
+      #      raise serializers.ValidationError("Tag name cannot contain characters like %!@#$^&*")
+       # return value
+    def validate_name(self, value):
+        if any(char in value for char in '%!&*#'):
+            raise serializers.ValidationError('name should not contain special characters')
+        return value
+
+    name = serializers.SerializerMethodField()
+
+
+   
+
+    def get_name(self, obj):
+        """Return the capitalized version of the tag's name."""
+        return obj.name.capitalize()
+
+  
+    class Meta:
+        model = Tag
+        fields = '__all__'
+        read_only_fields = ('id', )
